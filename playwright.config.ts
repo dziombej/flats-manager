a97d378e382fee60a76e9b57c6ab141a9ccbc010
Lukasz Dabrowski <lukasz.dabrowski@tomtom.com>
2026-01-05 17:51:46 +0100

    chore: preparation for e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env.test') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
