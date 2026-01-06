f9763796a15708a78e33f18fd68c7f74d06120af
Lukasz Dabrowski <lukasz.dabrowski@tomtom.com>
2026-01-06 17:20:10 +0100

    chore: workflow master.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import node from "@astrojs/node";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,7 +15,13 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: process.env.CF_PAGES
+    ? cloudflare({
+        platformProxy: {
+          enabled: true,
+        },
+      })
+    : node({
+        mode: "standalone",
+      }),
 });
